e43cd89658ee8f6832c1a580ac8af9a8fa0a29d5
Emilzon Jeronimo <emilzonujero@gmail.com>
2022-03-23 23:35:46 -0600

    Add: Validaciones al request de usuarios (#3)

--- Api/Controllers/Users/Requests/UpsertUserRequest.cs
+++ Api/Controllers/Users/Requests/UpsertUserRequest.cs
@@ -1,8 +1,16 @@
-﻿namespace Api.Controllers.Users.Requests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers.Users.Requests;
 
 public class UpsertUserRequest
 {
+    [Required(ErrorMessage = "Este campo es requerido")]
     public string UserName { get; set; }
+    
+    [Required(ErrorMessage = "Este campo es requerido")]
+    [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido")]
     public string Email { get; set; }
+
+    [MaxLength(15, ErrorMessage = "El número de caracteres debe ser menor a 15")]
     public string Phone { get; set; }
 }
