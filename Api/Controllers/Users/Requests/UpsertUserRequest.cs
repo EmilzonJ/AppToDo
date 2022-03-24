96ab63d2fecaafd74670c7ec570750f16558d51e
Emilzon Jeronimo <emilzonujero@gmail.com>
2022-03-23 22:37:14 -0600

    Add: Controller de usuarios (#1)

---
+++ Api/Controllers/Users/Requests/UpsertUserRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Api.Controllers.Users.Requests;
+
+public class UpsertUserRequest
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
---
+++ Api/Controllers/Users/UsersController.cs
@@ -0,0 +1,102 @@
+﻿using Api.Controllers.Users.Requests;
+using Api.Controllers.Users.Responses;
+using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Users;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class UsersController : ControllerBase
+{
+    private readonly IRepositoryBase<User, Guid> _repository;
+
+    public UsersController(IRepositoryBase<User, Guid> repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<UserResponse>>> GetAll()
+    {
+        var users = await _repository.GetAllAsync();
+        var response = users.Select(user => new UserResponse
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+            Phone = user.Phone
+        });
+
+        return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserResponse>> GetById(Guid id)
+    {
+        var user = await _repository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var response = new UserResponse
+        {
+            Id = user.Id,
+            UserName = user.UserName,
+            Email = user.Email,
+            Phone = user.Phone
+        };
+
+        return Ok(response);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Guid>> Create([FromBody] UpsertUserRequest request)
+    {
+        var user = new User
+        {
+            Id = Guid.NewGuid(),
+            UserName = request.UserName,
+            Email = request.Email,
+            Phone = request.Phone
+        };
+
+        await _repository.AddAsync(user);
+
+        return Ok(user.Id);
+    }
+    
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult> Update(Guid id, [FromBody] UpsertUserRequest request)
+    {
+        var user = await _repository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.UserName = request.UserName;
+        user.Email = request.Email;
+        user.Phone = request.Phone;
+
+        await _repository.UpdateAsync(user);
+
+        return Ok();
+    }
+    
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> Delete(Guid id)
+    {
+        var user = await _repository.GetByIdAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        await _repository.RemoveAsync(user);
+
+        return Ok();
+    }
+}
