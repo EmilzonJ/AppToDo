5b9727f66c8c005384b2ce5e6363d5f1032ceb2a
Jecidhz18 <57949609+Jecidhz18@users.noreply.github.com>
2022-03-24 00:22:52 -0600

    Agregar validaciones al requests de categoria (#4)
    * Add: Validaciones agregadas al request de categorias
    
    * Add: Validaciones agregadas al request de tareas

--- Api/Controllers/Categories/Requests/UpsertCategoryRequest.cs
+++ Api/Controllers/Categories/Requests/UpsertCategoryRequest.cs
@@ -1,7 +1,12 @@
-﻿namespace Api.Controllers.Categories.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Controllers.Categories.Requests
 {
     public class UpsertCategoryRequest
     {
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [MaxLength(50, ErrorMessage = "El número de caracteres debe ser menor a 50")]
+
         public string Name { get; set; }
     }
 }
--- Api/Controllers/Tasks/Requests/UpsertTaskRequest.cs
+++ Api/Controllers/Tasks/Requests/UpsertTaskRequest.cs
@@ -1,13 +1,21 @@
-﻿using TaskStatus = Domain.Enums.TaskStatus;
+﻿using System.ComponentModel.DataAnnotations;
+using TaskStatus = Domain.Enums.TaskStatus;
 
 namespace Api.Controllers.Tasks.Requests;
 
 public class UpsertTaskRequest
 {
+    [Required(ErrorMessage = "Este campo es requerido")]
     public string Name { get; set; }
+
+    [MaxLength(100, ErrorMessage = "El número de caracteres debe ser menor a 100")]
     public string Description { get; set; }
+
     public DateTime StartDate { get; set; }
+
     public DateTime EndDate { get; set; }
+
     public TaskStatus Status { get; set; }
+
     public Guid CategoryId { get; set; }
 }
